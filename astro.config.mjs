7794995023ace1a55ecf27c334760c1b137a901c
chloebaisse1 <baissechloe@gmail.com>
2025-03-11 09:46:18 +0400

    deploiement

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
-import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://chloebaisse1.github.io",
+  base: "Cv-portfolio",
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
